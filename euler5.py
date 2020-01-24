d6008cb53ae069f6484b4f1623e194d4cba8d335
Aaron <10zy11@gmail.com>
2020-01-24 11:09:18 -0500

    added solution to euler project problem 5

---
+++ euler5.py
@@ -0,0 +1,15 @@
+def smallest_multiple(num):
+    '''(int) -> int
+Finds the lowest number that has all numbers from 1 to num as factors
+'''
+    small = num
+    while True:
+        
+        for i in range(2,num+1):
+            if small % i == 0 and i == num:
+                return small
+            if small % i != 0:
+                small += num
+                break
+    
+        
