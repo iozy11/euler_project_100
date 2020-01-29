26a01c39e2be4b8e7d8ff8191fc33e9c4f5e3b29
Aaron <10zy11@gmail.com>
2020-01-29 13:17:25 -0500

    Added euler10.py

---
+++ euler10.py
@@ -0,0 +1,22 @@
+def prime_sieve(num):
+    '''(int) -> array
+Finds the sum of all primes smaller than num
+'''
+    #Create an array of "True" 2 through num
+    arrT = [True for i in range(2,num+1)]
+    arrP = []
+    p = 2
+    #Primes beneath a certain num all fall between 2 and sqrt(num)
+    while p * p < num:
+        #Set false all multiples of number p
+        for i in range(2*p,len(arrT),p):
+            arrT[i] = False
+        p += 1
+        #any number that is still "True" is prime
+    #Create an array, starting with 2, of all True indices(the index == a prime)
+    for i in range(2,len(arrT)):
+        if arrT[i] == True:
+            arrP.append(i)
+    return arrP
+
+print(sum(prime_sieve(2000000)))
